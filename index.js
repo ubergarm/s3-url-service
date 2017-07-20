34b7fd51136ba405e98eb894296c41d528bd4bff
John Leimgruber <leimgrub@gmail.com>
2017-07-20 17:16:12 -0400

    eslint standard

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 var AWS = require('aws-sdk')
-var restify = require('restify');
-var cookieParser = require('restify-cookies');
-var jwt = require('restify-jwt');
+var restify = require('restify')
+var cookieParser = require('restify-cookies')
+var jwt = require('restify-jwt')
 
 // configure restify server instance
-var server = restify.createServer({name: 's3-url-service'});
+var server = restify.createServer({name: 's3-url-service'})
 // configure JWT middleware
-server.use(restify.queryParser());
-server.use(cookieParser.parse);
+server.use(restify.queryParser())
+server.use(cookieParser.parse)
 
-server.use( jwt({
+server.use(jwt({
   // default secret is 'secret' --> YOU SHOULD SET A BETTER SECRET!
   secret: process.env.JWT_SECRET || 'secret',
   // default to provide a valid response even if no JWT credentails in request
   credentialsRequired: (process.env.JWT_CREDENTIALS_REQUIRED &&
-                        process.env.JWT_CREDENTIALS_REQUIRED.toLowerCase() == 'true') || false,
+                        process.env.JWT_CREDENTIALS_REQUIRED.toLowerCase() === true) || false,
   getToken: function fromHeaderOrQuerystringOrCookie (req) {
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-      return req.headers.authorization.split(' ')[1];
+      return req.headers.authorization.split(' ')[1]
     } else if (req.query && req.query.token) {
-      return req.query.token;
+      return req.query.token
     } else if (req.cookies && req.cookies['token']) {
-      return req.cookies['token'];
+      return req.cookies['token']
     }
-    return null;
+    return null
   }
-}));
+}))
 // configure AWS credentials
 AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION || 'us-east-1',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+})
 
 // pull in a global config variable for link and cache expiration (in seconds)
 var EXPIRES = parseInt(process.env.EXPIRES) || 2592000 // 30 days (in seconds)
@@ -39,40 +39,41 @@ var EXPIRES = parseInt(process.env.EXPIRES) || 2592000 // 30 days (in seconds)
 function requestHandler (method) {
   return function (req, res, next) {
     // handle Bearer token claims here
-    if(req.user) {
-      console.log("Bearer token claims:\n", req.user);
+    if (req.user) {
+      console.log('Bearer token claims:\n', req.user)
     }
 
     // check for valid bucket and key parameters
-    if(!req.params[0] || !req.params[1]) {
+    if (!req.params[0] || !req.params[1]) {
       return next(new Error('ERROR: Requires both valid bucket and key parameters'))
     }
-    var bucket = decodeURIComponent(req.params[0]);
-    var key = decodeURIComponent(req.params[1]);
+    var bucket = decodeURIComponent(req.params[0])
+    var key = decodeURIComponent(req.params[1])
 
     // generate presigned link
-    var s3 = new AWS.S3();
+    var s3 = new AWS.S3()
     var params = {
       Bucket: bucket,
       Key: key,
       Expires: EXPIRES
-    };
+    }
     s3.getSignedUrl(method, params, function (err, url) {
-      if (err)
-        return next(err);
-      res.cache({maxAge: EXPIRES});
-      res.redirect(307, url, next);
-    });
+      if (err) {
+        return next(err)
+      }
+      res.cache({maxAge: EXPIRES})
+      res.redirect(307, url, next)
+    })
   }
 }
 
 // S3 getObject redirect endpoint GET /:bucket/:key
-server.get(/^\/([a-zA-Z0-9_\.-]+)\/(.*)/, requestHandler('getObject'));
+server.get(/^\/([a-zA-Z0-9_\.-]+)\/(.*)/, requestHandler('getObject'))  // eslint-disable-line
 
 // S3 putObject redirect endpoint PUT /:bucket/:key
-server.put(/^\/([a-zA-Z0-9_\.-]+)\/(.*)/, requestHandler('putObject'));
+server.put(/^\/([a-zA-Z0-9_\.-]+)\/(.*)/, requestHandler('putObject'))  // eslint-disable-line
 
 // fire up the server
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url)
+})
